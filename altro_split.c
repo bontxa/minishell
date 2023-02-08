d2877a28f60fef2dc08dd3754abc995befbffe36
Alessio Boncinelli <aboncine@c2r4p12.42firenze.it>
2023-02-08 19:18:20 +0100

    refactor: scorporato altro_split.c speriamo bene

--- altro_split.c
+++ altro_split.c
@@ -1,84 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   altro_split.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboncine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 19:17:08 by aboncine          #+#    #+#             */
+/*   Updated: 2023/02/08 19:17:09 by aboncine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+void	ft_change_split_flags(char c, int *flagvirgo, int *flagapici)
+{
+	if (c == 34)
+	{
+		if (*flagvirgo == 0)
+			*flagvirgo = 1;
+		else
+			*flagvirgo = 0;
+	}
+	if (c == 39)
+	{
+		if (*flagapici == 0)
+			*flagapici = 1;
+		else
+			*flagapici = 0;
+	}
+}
 
 int	ft_how_many_words(char *s)
 {
 	int	i;
-	int	flagApici;
-	int	flagVirgo;
+	int	flagapici;
+	int	flagvirgo;
 	int	count;
 
 	i = 0;
-	flagApici = 0;
-	flagVirgo = 0;
+	flagapici = 0;
+	flagvirgo = 0;
 	count = 0;
 	while (s[i])
 	{
-		if (s[i] != ' ' && (i == 0 || (s[i - 1] == ' ' && flagApici == 0 && flagVirgo == 0)))
-		{
+		if (s[i] != ' ' && (i == 0 || (s[i - 1] == ' '
+					&& flagapici == 0 && flagvirgo == 0)))
 			count++;
-		}
-		if (s[i] == 34)
-		{
-			if (flagVirgo == 0)
-				flagVirgo = 1;
-			else
-				flagVirgo = 0;
-		}
-		if (s[i] == 39)
-		{
-			if (flagApici == 0)
-				flagApici = 1;
-			else
-				flagApici = 0;
-		}
+		if (s[i] == 34 || s[i] == 39)
+			ft_change_split_flags(s[i], &flagvirgo, &flagapici);
 		i++;
 	}
-	//printf("prima di splittare ho contato %d parole\n", count);
 	return (count);
+}
+
+void	ft_crea_parola_helper(char *s, int *i)
+{
+	int		flagapici;
+	int		flagvirgo;
+
+	flagapici = 0;
+	flagvirgo = 0;
+	while (s[*i])
+	{
+		if (s[*i] == ' ' && flagapici == 0 && flagvirgo == 0)
+		{
+			*i = *i - 1;
+			break ;
+		}
+		if (s[*i] == 34 || s[*i] == 39)
+			ft_change_split_flags(s[*i], &flagvirgo, &flagapici);
+		*i = *i + 1;
+	}
 }
 
 char	*ft_crea_parola(char *s, int i)
 {
 	int		b;
-	int		flagApici;
-	int		flagVirgo;
 	char	*res;
 	int		c;
 
-	flagApici = 0;
-	flagVirgo = 0;
 	b = i;
 	c = 0;
-	while (s[i])
-	{
-		if (s[i] == ' ' && flagApici == 0 && flagVirgo == 0)
-		{
-			i--;
-			break;
-		}
-		if (s[i] == 34)
-		{
-			if (flagVirgo == 0)
-				flagVirgo = 1;
-			else
-				flagVirgo = 0;
-		}
-		if (s[i] == 39)
-		{
-			if (flagApici == 0)
-				flagApici = 1;
-			else
-				flagApici = 0;
-		}
-		i++;
-	}
+	ft_crea_parola_helper(s, &i);
 	res = malloc(sizeof(char) * (i - b + 2));
 	while (b <= i)
-	{
-		res[c] = s[b];
-		b++;
-		c++;
-	}
+		res[c++] = s[b++];
 	res[c] = 0;
 	return (res);
 }
@@ -88,59 +94,23 @@ char	**ft_altro_split(char *s)
 	char	**res;
 	int		i;
 	int		b;
-	int		flagApici;
-	int		flagVirgo;
+	int		flagapici;
+	int		flagvirgo;
 
 	i = 0;
 	b = 0;
-	flagApici = 0;
-	flagVirgo = 0;
+	flagapici = 0;
+	flagvirgo = 0;
 	res = malloc (sizeof(char *) * (ft_how_many_words(s) + 1));
 	while (s[i])
 	{
-		if (s[i] != ' ' && (i == 0 || (s[i - 1] == ' ' && flagApici == 0 && flagVirgo == 0)))
-		{
-			res[b] = ft_crea_parola(s, i);
-			//printf("resb = %s\n", res[b]);
-			b++;
-		}
-		if (s[i] == 34)
-		{
-			if (flagVirgo == 0)
-				flagVirgo = 1;
-			else
-				flagVirgo = 0;
-		}
-		if (s[i] == 39)
-		{
-			if (flagApici == 0)
-				flagApici = 1;
-			else
-				flagApici = 0;
-		}
+		if (s[i] != ' ' && (i == 0 || (s[i - 1] == ' '
+					&& flagapici == 0 && flagvirgo == 0)))
+			res[b++] = ft_crea_parola(s, i);
+		if (s[i] == 34 || s[i] == 39)
+			ft_change_split_flags(s[i], &flagvirgo, &flagapici);
 		i++;
 	}
 	res[b] = 0;
-	// int x = 0;
-	// while (res[x] != 0)
-	// {
-	// 	printf("dallo split %s\n", res[x]);
-	// 	x++;
-	// }
 	return (res);
 }
-
-// int	main(int argc, char **argv)
-// {
-// 	char	**res;
-// 	int		i;
-
-// 	i = 0;
-// 	printf("%d\n", ft_how_many_words(argv[1]));
-// 	res = ft_altro_split(argv[1]);
-// 	while (res[i] != 0)
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// }
